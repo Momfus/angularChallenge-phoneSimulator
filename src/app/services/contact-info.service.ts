713395ff7f0dd91f81180309b86b2f8a37aab701
Momfus <momfus@outlook.com>
2023-04-02 15:29:45 -0300

    most of the contact service done

--- src/app/services/contact-info.service.ts
+++ src/app/services/contact-info.service.ts
@@ -1,9 +1,73 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Contact } from '../models/contact.model';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactInfoService {
 
-  constructor() { }
+  private readonly CONTACT_STORAGE_KEY = 'contacts';
+
+  private contacts$ = new BehaviorSubject<Contact[]>([]);
+
+  constructor() {
+    this.contacts$.next(JSON.parse(localStorage.getItem(this.CONTACT_STORAGE_KEY)  ?? '[]') );
+  }
+
+  private saveToLocalStorage(contacts: Contact[]): void {
+    localStorage.setItem(this.CONTACT_STORAGE_KEY, JSON.stringify(contacts));
+  }
+
+  getContacts(): Observable<Contact[]> {
+
+    return this.contacts$.asObservable();
+
+  }
+
+  createContact(contact: Contact): void {
+
+    const contactsList = this.contacts$.getValue();BehaviorSubject
+    const newContact: Contact = { ...contact, id: uuidv4() };
+
+    contactsList.push(newContact);
+    this.contacts$.next(contactsList);
+    this.saveToLocalStorage(contactsList);
+
+  }
+
+  updateContact(contact: Contact): void {
+
+    const contactsList = this.contacts$.getValue();
+    const index = contactsList.findIndex( c => c.id === contact.id);
+
+    if( index !== -1 ) {
+      contactsList[index] = contact;
+      this.contacts$.next(contactsList);
+      this.saveToLocalStorage(contactsList);
+    }
+
+  }
+
+  deleteContact(id: string): void {
+
+    const contactList = this.contacts$.getValue();
+    const index = contactList.findIndex( c => c.id === id);
+
+    if( index !== -1 ) {
+      contactList.splice(index, 1);
+      this.contacts$.next(contactList);
+      this.saveToLocalStorage(contactList);
+    }
+
+  }
+
+  getContactbyId(id: string): Contact | undefined {
+
+    const contactsList = this.contacts$.getValue();
+    return contactsList.find( c => c.id === id);
+
+  }
+
 }
