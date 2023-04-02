0ef02f365f644a2cfad515841b8d2d3c20e5eecf
Momfus <momfus@outlook.com>
2023-04-02 20:39:36 -0300

    fix grid paginated

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,8 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { PageEvent } from '@angular/material/paginator';
 import { Contact } from '../../models/contact.model';
 import { ContactInfoService } from '../../services/contact-info.service';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -16,20 +14,10 @@ export class HomeComponent implements OnInit {
 
   constructor(
     private contactService: ContactInfoService,
-    private breakpointObserver: BreakpointObserver
   ) {}
 
   ngOnInit(): void {
     this.setContactList();
-
-    this.breakpointObserver.observe([Breakpoints.Small, Breakpoints.XSmall]).subscribe(result => {
-      if (result.matches) {
-        this.gridCols = 1;
-      } else {
-        this.gridCols = 3;
-      }
-    });
-
   }
 
   setContactList(pageIndex: number = 0, pageSize: number = 9) {
